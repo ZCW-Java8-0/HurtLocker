a21b8a74937ad3c1a584ad3e75458371bbf9e5a2
L. Dolio Durant <thesleuth@brotherwithajob.com>
2022-04-11 08:19:38 -0400

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,17 @@
+import org.apache.commons.io.IOUtils;
+import java.io.IOException;
+
+public class Main {
+
+    public String readRawDataToString() throws Exception{
+        ClassLoader classLoader = getClass().getClassLoader();
+        String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
+        return result;
+    }
+
+    public static void main(String[] args) throws Exception{
+        String output = (new Main()).readRawDataToString();
+        System.out.println(output);
+
+    }
+}
